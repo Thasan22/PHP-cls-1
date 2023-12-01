4555428af7d6260771eba2a6cff72661e1d9fecd
Thasan22 <118618754+Thasan22@users.noreply.github.com>
2023-12-01 10:27:18 +0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+
+echo "My name is Mohammad Thasan. ";
+
+//With variable
+$name = "Mohammad Thasan";
+$age = 22;
+echo "My name is $name. I am $age years old. ";
+
+//CONSTANT
+define('DAY', 365);
+define('PI', 3.1416);
+echo "One year is equal to ";
+echo DAY;
+echo " days. ";
+echo PI;
